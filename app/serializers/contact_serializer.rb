512a67a87a35df7fb720c831048269957191278f
yuta-ushijima <register@yuta-u.com>
2018-06-28 00:00:40 +0900

    シリアライザーにおける関連付け

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,7 +1,12 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-   def attributes(*args)
+  # モデル同士の関連付け
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+
+  def attributes(*args)
     h = super(*args)
     # h[:birthday] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
     h[:birthday] =  object.birthdate.to_time.iso8601 unless object.birthdate.blank?
