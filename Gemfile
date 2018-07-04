af6944654f0799644827f21044ac90b94738ecfd
yuta-ushijima <register@yuta-u.com>
2018-07-05 00:39:33 +0900

    AMSについて/belongs_to has_many has_oneとroutesの関係

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'sqlite3'
 gem 'active_model_serializers', '~> 0.10.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
+# Manage Procfile-based applications
+gem 'foreman'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,10 +1,31 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate #, :author
 
   # モデル同士の関連付け
-  belongs_to :kind
-  has_many :phones
-  has_one :address
+  belongs_to :kind do
+    link(:related) { contact_kind_url(object.kind.id) }
+  end
+
+  has_many :phones do
+    link(:related) { contact_phones_url(object.id) }
+  end
+
+  has_one :address  do
+    link(:related) { contact_address_url(object.id) }
+  end
+
+  link(:self) { contact_url(object.id) }
+  link(:kind) { kind_url(object.kind.id) }
+  
+
+  # def author
+  #   "ウェブ系ウシジマくん"
+  # end
+
+  # meta do
+  #   { author: "ウェブ系ウシジマくん" }
+  # end
+
 
   def attributes(*args)
     h = super(*args)
