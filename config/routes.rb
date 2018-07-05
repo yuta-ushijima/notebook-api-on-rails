35fdf075f8ff760bbd389ceb85a0e6f6f813e6c3
yuta-ushijima <register@yuta-u.com>
2018-07-05 23:36:43 +0900

    has_oneにおけるAMSのCRUD操作

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
     resource :phones, only: [:show], path: 'relationships/phones'
 
     # Addressモデルとのネストされたroutes(has_one)
-    resource :address, only: [:show]
-    resource :address, only: [:show], path: 'relationships/address'
+    resource :address, only: [:show, :update, :create, :destroy]
+    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
